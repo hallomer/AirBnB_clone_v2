b9e25f9161b390f4bb12e2072e64f3fb0b5bdc77
Hiba <hebaaltayeb2@icloud.com>
2024-05-02 11:34:13 +0200

    what did i do

--- models/amenity.py
+++ models/amenity.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel):
     """represents an amenity"""
-    __tablename__ = 'amenities'
-    name = Column(String(128), nullable=False)
+    name = ""
--- models/place.py
+++ models/place.py
@@ -1,38 +1,24 @@
 #!/usr/bin/python3
 """This module defines a class Place"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
+from sqlalchemy import Column, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
 
 
-place_amenity = Table('place_amenity', Base.metadata,
-                      Column('place_id', String(60),
-                             ForeignKey('places.id', onupdate='CASCADE',
-                                        ondelete='CASCADE'),
-                             primary_key=True),
-                      Column('amenity_id', String(60),
-                             ForeignKey('amenities.id',
-                                        onupdate='CASCADE',
-                                        ondelete='CASCADE'),
-                             primary_key=True))
-
-
 class Place(BaseModel, Base):
-    """This class defines a place by various attributes"""
-    __tablename__ = 'places'
-    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=True)
-    number_rooms = Column(Integer, default=0, nullable=False)
-    number_bathrooms = Column(Integer, default=0, nullable=False)
-    max_guest = Column(Integer, default=0, nullable=False)
-    price_by_night = Column(Integer, default=0, nullable=False)
-    latitude = Column(Float, nullable=True)
-    longitude = Column(Float, nullable=True)
-    user = relationship("User", back_populates="places")
-    cities = relationship("City", back_populates="places")
-    reviews = relationship("Review", back_populates="place",
-                           cascade="all, delete, delete-orphan")
-    amenities = relationship("Amenity", secondary="place_amenity",
-                             backref="place_amenities", viewonly=False)
+"""This class defines a place by various attributes"""
+tablename = 'places'
+city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+name = Column(String(128), nullable=False)
+description = Column(String(1024), nullable=True)
+number_rooms = Column(Integer, default=0, nullable=False)
+number_bathrooms = Column(Integer, default=0, nullable=False)
+max_guest = Column(Integer, default=0, nullable=False)
+price_by_night = Column(Integer, default=0, nullable=False)
+latitude = Column(Float, nullable=True)
+longitude = Column(Float, nullable=True)
+user = relationship("User", back_populates="places")
+cities = relationship("City", back_populates="places")
+reviews = relationship("Review", back_populates="place",
+                       cascade="all, delete, delete-orphan")
