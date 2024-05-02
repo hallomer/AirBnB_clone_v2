2142927fdc5168816da5c2fea1b652c7f2b6b694
Hiba <hebaaltayeb2@icloud.com>
2024-05-02 11:36:07 +0200

    what did i do

--- models/place.py
+++ models/place.py
@@ -6,19 +6,19 @@ from sqlalchemy.orm import relationship
 
 
 class Place(BaseModel, Base):
-"""This class defines a place by various attributes"""
-tablename = 'places'
-city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-name = Column(String(128), nullable=False)
-description = Column(String(1024), nullable=True)
-number_rooms = Column(Integer, default=0, nullable=False)
-number_bathrooms = Column(Integer, default=0, nullable=False)
-max_guest = Column(Integer, default=0, nullable=False)
-price_by_night = Column(Integer, default=0, nullable=False)
-latitude = Column(Float, nullable=True)
-longitude = Column(Float, nullable=True)
-user = relationship("User", back_populates="places")
-cities = relationship("City", back_populates="places")
-reviews = relationship("Review", back_populates="place",
-                       cascade="all, delete, delete-orphan")
+    """This class defines a place by various attributes"""
+    tablename = 'places'
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=True)
+    number_rooms = Column(Integer, default=0, nullable=False)
+    number_bathrooms = Column(Integer, default=0, nullable=False)
+    max_guest = Column(Integer, default=0, nullable=False)
+    price_by_night = Column(Integer, default=0, nullable=False)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
+    user = relationship("User", back_populates="places")
+    cities = relationship("City", back_populates="places")
+    reviews = relationship("Review", back_populates="place",
+                        cascade="all, delete, delete-orphan")
