6bc5d7c1c8ee1f83348ef69a5c989af0c5b4e57b
Hiba <hebaaltayeb2@icloud.com>
2024-05-02 12:54:50 +0200

    bye

--- models/base_model.py
+++ models/base_model.py
@@ -56,8 +56,16 @@ class BaseModel:
         """Convert instance into dict format"""
         dictionary = self.__dict__.copy()
         dictionary['__class__'] = self.__class__.__name__
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
+        dictionary['created_at'] = (
+            self.created_at.isoformat()
+            if isinstance(self.created_at, datetime)
+            else self.created_at
+        )
+        dictionary['updated_at'] = (
+            self.updated_at.isoformat()
+            if isinstance(self.updated_at, datetime)
+            else self.updated_at
+        )
         if '_sa_instance_state' in dictionary:
             del dictionary['_sa_instance_state']
         return dictionary
