44521c14376bd493fd8342e1f15554eb0467d9e1
Hiba <hebaaltayeb2@icloud.com>
2024-06-01 00:07:23 +0300

    pycodestyle

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -10,10 +10,12 @@ def hello():
     """Route handler for the root URL."""
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-     """Route handler for the /hbnb URL."""
-     return "HBNB"
+    """Route handler for the /hbnb URL."""
+    return "HBNB"
+
 
 if __name__ == '__main__':
-     app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
