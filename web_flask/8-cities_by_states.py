cabfa688a43c4f8dd751cd31859f2c681cd8f808
Hiba <hebaaltayeb2@icloud.com>
2024-06-03 03:09:52 +0300

    pycode

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -12,6 +12,7 @@ def cities_by_states():
     states = storage.all("State")
     return render_template("8-cities_by_states.html", states=states)
 
+
 @app.teardown_appcontext
 def teardown_db(exception):
     """Closes the storage on teardown of the app context."""
