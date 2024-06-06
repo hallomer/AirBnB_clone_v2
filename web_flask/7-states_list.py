42a9ced2e923299dbd073a77395d759a1390fa8f
hiba <hebaaltayeb2@icloud.com>
2024-06-06 05:01:54 +0300

    11

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -10,7 +10,8 @@ app = Flask(__name__)
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """Route handler for /states_list URL."""
-    states = storage.all(State)
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda state: state.name)
     return render_template('7-states_list.html', states=states)
 
 
